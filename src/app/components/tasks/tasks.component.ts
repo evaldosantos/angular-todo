a29fd339ad1142a1d760833f74309397ecb670f0
Evaldo Santos <jesantosjr@protonmail.com>
2022-02-12 22:14:46 -0300

    toggle reminder

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -25,4 +25,9 @@ export class TasksComponent implements OnInit {
     });
   }
 
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.toggleReminder(task).subscribe();
+  }
+
 }
